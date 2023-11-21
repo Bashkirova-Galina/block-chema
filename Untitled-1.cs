cd8c4f39349cbcb5db3a0ce862b2927c79ebce99
Galina <bright_emerald@mail.ru>
2023-11-21 20:59:23 +0300

    add

---
+++ Untitled-1.cs
@@ -0,0 +1,9 @@
+Console.Wirte("Введите число: ");
+// int n =Convert.ToInt32(Console.ReadLine());
+Console.Wirte("Введите число: ");
+// int n =Convert.ToInt32(Console.ReadLine());
+if (a>b){
+    Console.WriteLine(a);
+}
+else
+    Console.WriteLine(b);
